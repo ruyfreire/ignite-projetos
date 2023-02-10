bb470b9c0da7b4d862e38647b2cd57a28dbe69db
Ruy Freire <ruy.slv@gmail.com>
2023-02-09 23:10:30 -0300

    header completo

--- react/desafio-1/todo-list/src/App.tsx
+++ react/desafio-1/todo-list/src/App.tsx
@@ -1,7 +1,46 @@
+import { PlusCircle } from 'phosphor-react'
+
+import Logo from './assets/logo.svg'
+
+import styles from './App.module.css'
+
+import './global.css'
+
 export function App() {
   return (
     <div>
-      <h1>React</h1>
+      <header className={styles.header}>
+        <img src={Logo} alt="Logo" />
+      </header>
+
+      <main>
+        <div className={styles.search}>
+          <input type="text" placeholder='Adicione uma nova tarefa' />
+          <button>Criar <PlusCircle size={20} /></button>
+        </div>
+
+        <div className={styles.resume}>
+          <div className={styles.tasks}>
+            <p>Tarefas criadas</p>
+            <span>5</span>
+          </div>
+
+          <div className={styles.tasksComplete}>
+            <p>Concluídas</p>
+            <span>2 de 5</span>
+          </div>
+        </div>
+
+        <div>
+          <div className={styles.card}>
+            <input type="radio" name="todo" id="todo" />
+            <p>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
+            <button title='Excluir tarefa'>
+              X
+            </button>
+          </div>
+        </div>
+      </main>
     </div>
   )
 }
