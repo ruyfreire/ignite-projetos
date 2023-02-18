c343cea5cb5adde5b8e6a8cf7efee08d4dca5d75
Ruy Freire <ruy.slv@gmail.com>
2023-02-18 14:18:09 -0300

    desafio completo

---
+++ react/desafio-1/todo-list/src/components/Task.tsx
@@ -0,0 +1,39 @@
+import { HTMLAttributes } from 'react'
+import { Check, Trash } from 'phosphor-react'
+import clsx from 'clsx'
+
+import { ITask } from '../App'
+
+import styles from './Task.module.css'
+
+interface TaskProps extends HTMLAttributes<HTMLLabelElement> {
+  task: ITask
+  deleting?: boolean
+  onChangeStatus: (checked: boolean) => void
+  onDelete: (id: ITask['id']) => void
+}
+
+export function Task({ task, onChangeStatus, onDelete, deleting, ...props }: TaskProps) {
+  const { id, completed, content } = task
+
+  return (
+    <label {...props} htmlFor={id} className={clsx(styles.task, {
+      [styles.taskCompleted]: completed,
+      [styles.taskDeleting]: deleting
+    })}>
+      <input type="checkbox" id={id} checked={completed} onChange={(event) => onChangeStatus(event.target.checked)} />
+
+      <div className={styles.check}>
+        <label htmlFor={id} className={styles.checkbox}>
+          <Check size={12} />
+        </label>
+      </div>
+
+      <p>{content}</p>
+
+      <button title='Excluir tarefa' onClick={() => onDelete(id)}>
+        <Trash size={16} />
+      </button>
+    </label>
+  )
+}
