7eadb1c563f9a4bd95057a4646aefba03b76f3d3
Ruy Freire <ruy.slv@gmail.com>
2023-04-18 22:56:07 -0300

    revert rotas

--- react/desafio-2/coffee-delivery/src/routes/index.tsx
+++ react/desafio-2/coffee-delivery/src/routes/index.tsx
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import { Checkout } from '../pages/Checkout'
 import { Home } from '../pages/Home'
 import { Delivery } from '../pages/Delivery'
-import { Header } from '../components/Header'
+import { Main } from '../template/Main'
 
 export const routesList = {
   home: '/',
@@ -13,13 +13,12 @@ export const routesList = {
 
 export function Router() {
   return (
-    <div>
-      <Header />
-      <Routes>
+    <Routes>
+      <Route path="/" element={<Main />}>
         <Route path={routesList.home} element={<Home />} />
         <Route path={routesList.checkout} element={<Checkout />} />
         <Route path={routesList.checkoutSuccess} element={<Delivery />} />
-      </Routes>
-    </div>
+      </Route>
+    </Routes>
   )
 }
