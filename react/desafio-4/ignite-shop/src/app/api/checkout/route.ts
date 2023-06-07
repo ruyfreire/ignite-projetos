3eadec98433d96bad5ea207761a4418e25e2f210
Ruy Freire <ruy.slv@gmail.com>
2023-06-06 22:42:24 -0300

    try catch

--- react/desafio-4/ignite-shop/src/app/api/checkout/route.ts
+++ react/desafio-4/ignite-shop/src/app/api/checkout/route.ts
@@ -4,34 +4,39 @@ import { stripe } from '@/service/stripe'
 import { ProductCheckout } from '@/types/Product'
 
 export async function POST(req: Request) {
-  const { productsCheckout } = (await req.json()) as {
-    productsCheckout: ProductCheckout[]
-  }
+  try {
+    const { productsCheckout } = (await req.json()) as {
+      productsCheckout: ProductCheckout[]
+    }
 
-  if (!productsCheckout || productsCheckout.length === 0) {
+    if (!productsCheckout || productsCheckout.length === 0) {
+      return NextResponse.json(
+        { error: 'Product checkout is required' },
+        { status: 400 }
+      )
+    }
+
+    const successUrl = `${process.env.VERCEL_URL}/success?session_id={CHECKOUT_SESSION_ID}`
+    const cancelUrl = `${process.env.VERCEL_URL}/`
+
+    const lineItems = productsCheckout.map((product) => ({
+      price: product.priceId,
+      quantity: product.quantity,
+    }))
+
+    const checkoutSession = await stripe.checkout.sessions.create({
+      success_url: successUrl,
+      cancel_url: cancelUrl,
+      mode: 'payment',
+      line_items: lineItems,
+    })
+
     return NextResponse.json(
-      { error: 'Product checkout is required' },
-      { status: 400 }
+      { checkoutUrl: checkoutSession.url },
+      { status: 201 }
     )
+  } catch (error) {
+    console.error(error)
+    throw error
   }
-
-  const successUrl = `${process.env.VERCEL_URL}/success?session_id={CHECKOUT_SESSION_ID}`
-  const cancelUrl = `${process.env.VERCEL_URL}/`
-
-  const lineItems = productsCheckout.map((product) => ({
-    price: product.priceId,
-    quantity: product.quantity,
-  }))
-
-  const checkoutSession = await stripe.checkout.sessions.create({
-    success_url: successUrl,
-    cancel_url: cancelUrl,
-    mode: 'payment',
-    line_items: lineItems,
-  })
-
-  return NextResponse.json(
-    { checkoutUrl: checkoutSession.url },
-    { status: 201 }
-  )
 }
