4636954ff0df200327f85c805fb02691f970e0c2
Ruy Freire <ruy.slv@gmail.com>
2023-07-25 23:48:26 -0300

    adicionado provider google

--- react/desafio-6/book-wise/src/app/api/auth/[...nextauth]/authOptions.ts
+++ react/desafio-6/book-wise/src/app/api/auth/[...nextauth]/authOptions.ts
@@ -2,6 +2,7 @@ import { prisma } from "@/lib/prisma"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import { AuthOptions } from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google"
 
 export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
@@ -9,6 +10,12 @@ export const authOptions: AuthOptions = {
     GithubProvider({
       clientId: process.env.GITHUB_ID || "",
       clientSecret: process.env.GITHUB_SECRET || "",
+      allowDangerousEmailAccountLinking: true,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID || "",
+      clientSecret: process.env.GOOGLE_SECRET || "",
+      allowDangerousEmailAccountLinking: true,
     }),
   ],
 }
