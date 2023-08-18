a67b9f4919743b298ff0b3fff4e7ebedc10933c3
Ruy Freire <ruy.slv@gmail.com>
2023-08-17 21:18:49 -0300

    pagina de perfil

--- react/desafio-6/book-wise/src/app/(home)/(dashboard)/perfil/page.tsx
+++ react/desafio-6/book-wise/src/app/(home)/(dashboard)/perfil/page.tsx
@@ -2,45 +2,72 @@
 
 import { LastReview } from "@/@types/review"
 import { Input } from "@/components/Input"
+import { Loader } from "@/components/Loader"
 import { Rating } from "@/components/Rating"
 import { Title } from "@/components/Title"
 import { api } from "@/lib/axios"
+import { dayjs } from "@/lib/dayjs"
+import { capitalizeString } from "@/utils/string"
 import { ChevronLeft, User2 } from "lucide-react"
-import { useSession } from "next-auth/react"
 import Image from "next/image"
 import Link from "next/link"
 import { useSearchParams } from "next/navigation"
-import { useEffect } from "react"
+import { useCallback, useEffect, useRef, useState } from "react"
+
+interface SearchParams {
+  search?: string
+  limit: number
+}
 
 export default function Perfil() {
+  const debounceSearch = useRef<NodeJS.Timeout>()
   const params = useSearchParams()
   const user_id = params.get("user_id")
-  const session = useSession()
-  const isAuthenticated = session.status === "authenticated"
 
-  useEffect(() => {
-    async function getProfile() {
+  const [lastReviews, setLastReviews] = useState<LastReview[] | null>(null)
+
+  const searchBooks = useCallback(
+    async function (search?: string) {
       try {
-        let url = "profile/me"
+        const searchParams: SearchParams = { limit: 5 }
+        if (search) searchParams.search = search
 
-        if (user_id) {
-          url = `profile/${user_id}`
-        }
+        const { data } = await api.get<{ last_reviews: LastReview[] }>(
+          `reviews/${user_id || "me"}`,
+          { params: searchParams },
+        )
 
-        const { data } = await api.get<{ user: LastReview[] }>(url, {
-          params: { limit: 10 },
-        })
-
-        console.log("data", data)
+        setLastReviews(data.last_reviews || [])
       } catch (error) {
+        setLastReviews([])
         console.error(error)
       }
-    }
+    },
+    [user_id],
+  )
 
-    if (isAuthenticated) {
-      getProfile()
-    }
-  }, [isAuthenticated, user_id])
+  function handleSearchBook(event: React.ChangeEvent<HTMLInputElement>) {
+    clearTimeout(debounceSearch.current)
+
+    debounceSearch.current = setTimeout(() => {
+      const { value } = event.target
+      searchBooks(value)
+    }, 500)
+  }
+
+  function getDate(date: string) {
+    const objectDate = dayjs(date)
+
+    const distanceDate = objectDate.fromNow()
+    const formattedDate = capitalizeString(distanceDate)
+    const utcDate = capitalizeString(objectDate.format("LLLL"))
+
+    return { formattedDate, utcDate }
+  }
+
+  useEffect(() => {
+    searchBooks()
+  }, [searchBooks])
 
   return (
     <>
@@ -59,76 +86,62 @@ export default function Perfil() {
       )}
 
       <main className="flex flex-col gap-6">
-        <Input className="mb-2" placeholder="Buscar livro avaliado" />
+        <Input
+          className="mb-2"
+          onChange={handleSearchBook}
+          placeholder="Buscar livro avaliado"
+        />
 
-        <div>
-          <h5 className="mb-2 text-sm leading-relaxed text-gray-300">
-            Há 2 dias
-          </h5>
+        {lastReviews === null ? (
+          <Loader active />
+        ) : lastReviews.length === 0 ? (
+          <p className="text-center">Nenhuma avaliação feita pelo usuário</p>
+        ) : (
+          <ul className="flex flex-col gap-6">
+            {lastReviews.map((review) => {
+              const { formattedDate, utcDate } = getDate(review.created_at)
 
-          <div className="grid grid-cols-[98px_1fr] grid-rows-[134px_1fr] gap-6 rounded-lg bg-gray-700 p-6">
-            <Image
-              src="/books/entendendo-algoritmos.png"
-              alt="Entendendo Algoritmos"
-              width={98}
-              height={134}
-              className="rounded"
-            />
+              return (
+                <li key={review.id}>
+                  <div>
+                    <time
+                      title={utcDate}
+                      dateTime={review.created_at}
+                      className="mb-2 block text-sm leading-relaxed text-gray-300"
+                    >
+                      {formattedDate}
+                    </time>
 
-            <div className="flex flex-col">
-              <h4 className="text-lg font-bold text-gray-100">
-                Entendendo Algoritmos
-              </h4>
-              <p className="text-sm leading-relaxed text-gray-400">
-                Aditya Bhargava
-              </p>
-              <Rating rating={4} className="mt-auto" />
-            </div>
+                    <div className="grid grid-cols-[98px_1fr] grid-rows-[134px_1fr] gap-6 rounded-lg bg-gray-700 p-6">
+                      <Image
+                        src={review.book.imageUrl}
+                        alt={review.book.title}
+                        width={98}
+                        height={134}
+                        className="rounded"
+                      />
 
-            <p className="col-span-2 text-sm leading-relaxed text-gray-300">
-              Tristique massa sed enim lacinia odio. Congue ut faucibus nunc
-              vitae non. Nam feugiat vel morbi viverra vitae mi. Vitae fringilla
-              ut et suspendisse enim suspendisse vitae. Leo non eget lacus
-              sollicitudin tristique pretium quam. Mollis et luctus amet sed
-              convallis varius massa sagittis. Proin sed proin at leo quis ac
-              sem. Nam donec accumsan curabitur amet tortor quam sit. Bibendum
-              enim sit dui lorem urna amet elit rhoncus ut. Aliquet euismod
-              vitae ut turpis. Aliquam amet integer pellentesque.
-            </p>
-          </div>
-        </div>
+                      <div className="flex flex-col">
+                        <h4 className="text-lg font-bold text-gray-100">
+                          {review.book.title}
+                        </h4>
+                        <p className="text-sm leading-relaxed text-gray-400">
+                          {review.book.author}
+                        </p>
+                        <Rating rating={4} className="mt-auto" />
+                      </div>
 
-        <div>
-          <h5 className="mb-2 text-sm leading-relaxed text-gray-300">
-            Há 4 meses
-          </h5>
-
-          <div className="grid grid-cols-[98px_1fr] grid-rows-[134px_1fr] gap-6 rounded-lg bg-gray-700 p-6">
-            <Image
-              src="/books/o-hobbit.png"
-              alt="O Hobbit"
-              width={98}
-              height={134}
-              className="rounded"
-            />
-
-            <div className="flex flex-col">
-              <h4 className="text-lg font-bold text-gray-100">O Hobbit</h4>
-              <p className="text-sm leading-relaxed text-gray-400">
-                J.R.R. Tolkien
-              </p>
-              <Rating rating={4} className="mt-auto" />
-            </div>
-
-            <p className="col-span-2 text-sm leading-relaxed text-gray-300">
-              Nec tempor nunc in egestas. Euismod nisi eleifend at et in
-              sagittis. Penatibus id vestibulum imperdiet a at imperdiet.
-            </p>
-          </div>
-        </div>
+                      <p className="col-span-2 text-sm leading-relaxed text-gray-300">
+                        {review.description}
+                      </p>
+                    </div>
+                  </div>
+                </li>
+              )
+            })}
+          </ul>
+        )}
       </main>
-
-      {/* <AsideProfile /> */}
     </>
   )
 }
