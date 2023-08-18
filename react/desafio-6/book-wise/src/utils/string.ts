a339a3298188e7bcacb703c27636a0b4e9103e30
Ruy Freire <ruy.slv@gmail.com>
2023-08-17 21:16:50 -0300

    utilitarios - sanitizar e normalizar strings

--- react/desafio-6/book-wise/src/utils/string.ts
+++ react/desafio-6/book-wise/src/utils/string.ts
@@ -1,5 +1,24 @@
+function isString(str: any) {
+  return typeof str === "string"
+}
+
 export function capitalizeString(str: string) {
-  if (typeof str !== "string") return str
+  if (!isString(str)) return str
 
   return str.charAt(0).toUpperCase().concat(str.slice(1))
+}
+
+export function normalizeString(str: string) {
+  if (!isString(str)) return str
+
+  return str
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .toLowerCase()
+}
+
+export function sanitizeString(str: string) {
+  if (!isString(str)) return str
+
+  return str.replace(/[^a-zA-Z0-9 -.,]/g, "")
 }
