397070888946bb2e96bf6e01341054bf304fde52
Ruy Freire <ruy.slv@gmail.com>
2023-08-28 22:33:53 -0300

    envs

--- react/desafio-6/book-wise/next.config.js
+++ react/desafio-6/book-wise/next.config.js
@@ -25,18 +25,8 @@ const nextConfig = {
           {
             key: "Access-Control-Allow-Origin",
             value:
-              process.env.NODE_ENV === "production"
-                ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-                : "*",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+              process.env.NEXT_PUBLIC_CONTROL_ORIGIN ||
+              `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`,
           },
         ],
       },
