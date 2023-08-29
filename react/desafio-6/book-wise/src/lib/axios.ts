34e452ce69122aab1325f56a68ead9d751e3e9a9
Ruy Freire <ruy.slv@gmail.com>
2023-08-28 22:02:48 -0300

    base url

--- react/desafio-6/book-wise/src/lib/axios.ts
+++ react/desafio-6/book-wise/src/lib/axios.ts
@@ -1,7 +1,5 @@
 import axios from "axios"
 
 export const api = axios.create({
-  baseURL:
-    process.env.API_BASE_URL ||
-    `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api`,
+  baseURL: "/api",
 })
