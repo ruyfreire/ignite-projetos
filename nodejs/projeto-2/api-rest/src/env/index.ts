7828c23bbe43dfcb23031a8fd3382b25699d089e
Ruy Freire <ruy.slv@gmail.com>
2023-09-28 23:47:19 -0300

    aula 1 e 2 do projeto completa

---
+++ nodejs/projeto-2/api-rest/src/env/index.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod'
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['development', 'production', 'test']).default('production'),
+  PORT: z.number().default(3333),
+  DATABASE_URL: z.string(),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (!_env.success) {
+  console.error('⚠️ Invalid environment variables:\n', _env.error.format())
+  process.exit(1)
+}
+
+export const env = _env.data
---
+++ nodejs/projeto-2/api-rest/src/server.ts
@@ -0,0 +1,13 @@
+import fastify from 'fastify'
+import { knex } from './database'
+import { env } from './env'
+
+const app = fastify()
+
+app.get('/', async () => {
+  const transactions = await knex('transactions').select('*')
+
+  return transactions
+})
+
+app.listen({ port: env.PORT }).then(() => console.log('HTTP server running!'))
