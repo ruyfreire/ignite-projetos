e5a9d2deadc3193ee820bbff2db2fd3d6b2ed06b
Ruy Freire <ruy.slv@gmail.com>
2023-10-08 17:03:30 -0300

    define host

--- nodejs/projeto-2/api-rest/src/env/index.ts
+++ nodejs/projeto-2/api-rest/src/env/index.ts
@@ -10,6 +10,7 @@ if (process.env.NODE_ENV === 'test') {
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'production', 'test']).default('production'),
   PORT: z.coerce.number().default(3333),
+  HOST: z.string().default('localhost'),
   DATABASE_URL: z.string(),
   DATABASE_CLIENT: z.enum(['sqlite3', 'pg']),
 })
