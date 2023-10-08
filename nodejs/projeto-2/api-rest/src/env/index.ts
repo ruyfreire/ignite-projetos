39a621a32d13a49c6c4fbddf405518b101ac1123
Ruy Freire <ruy.slv@gmail.com>
2023-10-08 16:13:00 -0300

    configs database

--- nodejs/projeto-2/api-rest/src/env/index.ts
+++ nodejs/projeto-2/api-rest/src/env/index.ts
@@ -9,8 +9,9 @@ if (process.env.NODE_ENV === 'test') {
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'production', 'test']).default('production'),
-  PORT: z.number().default(3333),
+  PORT: z.coerce.number().default(3333),
   DATABASE_URL: z.string(),
+  DATABASE_CLIENT: z.enum(['sqlite3', 'pg']),
 })
 
 const _env = envSchema.safeParse(process.env)
