76642cdebe03d39adae36ddeace57421cac7d917
Ruy Freire <ruy.slv@gmail.com>
2024-01-19 21:19:31 -0300

    configuração de testes e2e

--- nodejs/desafio-4/nest-fastfeet/src/infra/env/env.ts
+++ nodejs/desafio-4/nest-fastfeet/src/infra/env/env.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod'
 
 export const envSchema = z.object({
-  // DATABASE_URL: z.string().url(),
+  DATABASE_URL: z.string().url(),
   JWT_PRIVATE_KEY: z.string(),
   JWT_PUBLIC_KEY: z.string(),
   // CLOUDFLARE_ACCOUNT_ID: z.string(),
--- nodejs/desafio-4/nest-fastfeet/vitest.config.e2e.ts
+++ nodejs/desafio-4/nest-fastfeet/vitest.config.e2e.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     include: ['**/*.e2e-spec.ts'],
     globals: true,
     root: './',
-    setupFiles: ['./test/setup-e2e.ts'],
+    setupFiles: ['./tests/setup-e2e.ts'],
   },
   plugins: [
     tsConfigPaths(),
